075e4aa18c0b7385fd6a50eb8df6a69ae9aa1d32
vhollo <vhollo@protonmail.com>
2024-02-02 20:30:11 +0100

    Prismic: Slice Machine

---
+++ src/lib/prismicio.js
@@ -0,0 +1,46 @@
+import * as prismic from "@prismicio/client";
+import { enableAutoPreviews } from "@prismicio/svelte/kit";
+import config from "../../slicemachine.config.json";
+
+/**
+ * The project's Prismic repository name.
+ */
+export const repositoryName =
+  import.meta.env.VITE_PRISMIC_ENVIRONMENT || config.repositoryName;
+
+/**
+ * A list of Route Resolver objects that define how a document's `url` field is resolved.
+ *
+ * {@link https://prismic.io/docs/route-resolver#route-resolver}
+ *
+ * @type {prismic.ClientConfig["routes"]}
+ */
+// TODO: Update the routes array to match your project's route structure.
+const routes = [
+  // Examples:
+  // {
+  // 	type: "homepage",
+  // 	path: "/",
+  // },
+  // {
+  // 	type: "page",
+  // 	path: "/:uid",
+  // },
+];
+
+/**
+ * Creates a Prismic client for the project's repository. The client is used to
+ * query content from the Prismic API.
+ *
+ * @param {import('@prismicio/svelte/kit').CreateClientConfig} config - Configuration for the Prismic client.
+ */
+export const createClient = ({ cookies, ...config } = {}) => {
+  const client = prismic.createClient(repositoryName, {
+    routes,
+    ...config,
+  });
+
+  enableAutoPreviews({ client, cookies });
+
+  return client;
+};
