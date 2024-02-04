667b286cbaf7bdc77304d659610974c98a2b0ea4
vhollo <vhollo@protonmail.com>
2024-02-04 21:58:01 +0100

    grid 0/1/2/full

--- src/routes/[[preview=preview]]/+page.svelte
+++ src/routes/[[preview=preview]]/+page.svelte
@@ -8,11 +8,19 @@
   console.log(data.page)
 </script>
 
-<div class="grid justify-items-center md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 grid-flow-row-dense gap-12" class:full={data.page.data.preview_size}>
+<!--<header class="flex flex-row">
+  <figure>
+    <PrismicImage field={data.boxes_group.page_image} />
+  </figure>
+</header>-->
+<div class="grid justify-items-center sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 grid-flow-row-dense gap-12" class:full={data.page.data.preview_size}>
   {#each data.page.data.boxes_group as group}
 
-  <div class="card w-auto bg-base-300 shadow-xl col-span-{group.block_size}">
-    <figure class="w-100 h-96"><img class="object-fill" src={group.page_image.url} alt={group.page_image.alt} /></figure>
+  <div class="card w-auto bg-base-300 shadow-xl xs:col-span-full col-span-{group.block_size}">
+    <figure class="w-100 h-96">
+      <!--<img class="object-fill" src={group.page_image.url} alt={group.page_image.alt} />-->
+      <PrismicImage field={group.page_image} />
+    </figure>
     <div class="card-body">
       <h3>{group.page_tagline}</h3>
       <h2 class="card-title">
@@ -33,7 +41,7 @@
         >
       </PrismicRichText>
     </details>
-</div>
+    </div>
   </div>
 
   {/each}
@@ -61,7 +69,7 @@
         >
       </PrismicRichText>
     </details>
-</div>
+    </div>
   </div>
 
   {/each}
@@ -111,13 +119,21 @@
         <div class="badge badge-outline">Fashion</div> 
         <div class="badge badge-outline">Products</div>
       </div>
-      <details>
+      <!--<details>
         <summary>Tovább…</summary>
         <PrismicRichText
         field={group.page_body}
         >
       </PrismicRichText>
-    </details>
+    </details>-->
+    <div tabindex="0" class="collapse bg-base-100"> 
+      <div class="collapse-title text-xl font-medium">
+        Tovább…
+      </div>
+      <div class="collapse-content"> 
+        <PrismicRichText field={group.page_body}></PrismicRichText>
+      </div>
+    </div>
 
     </div>
   </div>
@@ -126,3 +142,13 @@
 </div>
 
 <SliceZone slices={data.page.data.slices} {components} />
+
+<style>
+  .col-span-full figure {
+    @apply h-5/6;
+  }
+  .col-span-0 {
+    display: none;
+    visibility: hidden;
+  }
+</style>
